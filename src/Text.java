715fe961e144ceb04fffcbb6f988bfbb01747908
GayBoiJarleru <jarl.bergstrom@elev.ga.ntig.se>
2020-03-13 11:03:28 +0100

    Öppna nästan klart

--- src/Text.java
+++ src/Text.java
@@ -1,6 +1,25 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Text {
+    public Text() {
+
+        sparaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JFileChooser fc =new JFileChooser();
+                int resultat = fc.showOpenDialog(null);
+                if (resultat != JFileChooser.APPROVE_OPTION) {
+                System.out.println("ingen fil valdes");
+                System.exit(0);
+
+
+                }}
+            }};
+
+}
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("Text");
         frame.setContentPane(new Text().Panel);
@@ -9,9 +28,9 @@ public class Text {
         frame.setVisible(true);
     }
 
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
+    private JButton nyttButton;
+    private JButton sparaButton;
+    private JButton darkModeButton;
     private JTextArea textArea1;
     private JPanel Panel;
 }
