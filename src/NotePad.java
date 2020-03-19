435869d90d40cce8ee05a119fa49a28148b7dec8
GayBoiJarleru <jarl.bergstrom@elev.ga.ntig.se>
2020-03-19 11:19:38 +0100

    Inget funkar

--- src/NotePad.java
+++ src/NotePad.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.
+import java.io.FileFilter*;
+import java.util.Scanner*;
+
 
 public class Text {
     public Text() {
@@ -8,18 +12,16 @@ public class Text {
         sparaButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                JFileChooser fc =new JFileChooser();
+                JFileChooser fc = new JFileChooser();
                 int resultat = fc.showOpenDialog(null);
                 if (resultat != JFileChooser.APPROVE_OPTION) {
-                System.out.println("ingen fil valdes");
-                System.exit(0);
-
-
-                }}
-            }};
+                    System.out.println("ingen fil valdes");
+                    System.exit(0);
+                }
+            }
+        }}; /* <- vad fan händer här */
 
 }
-
     public static void main(String[] args) {
         JFrame frame = new JFrame("Text");
         frame.setContentPane(new Text().Panel);
